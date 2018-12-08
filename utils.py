f82bc0769ebc76a649987f77b4d06256f2528060
Alexandre Spaeth <alexandre@e-180.com>
2018-12-08 15:48:37 +0100

    Improve utils file

--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
-def data_import(filename, cast=str):
+def data_import(filename, cast=str, split_line=False):
     data = []
     with open(filename) as file:
         line = file.readline()
         while line:
             if line.strip():
-                data.append(cast(line.strip()))
+
+                if split_line:
+                    line = line.split()
+                    data.append([cast(item.strip()) for item in line])
+                else:
+                    data.append(cast(item.strip()) for item in line)
+
             line = file.readline()
     return data
