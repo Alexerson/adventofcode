d6f5c98a3cdd73bf14a16de1507c373ab196f8d8
Alexandre Spaeth <alexandre@e-180.com>
2018-12-08 14:27:40 +0100

    Add 2017 day 4

---
+++ 2017/day4.py
@@ -0,0 +1,27 @@
+from utils import data_import
+
+
+def is_valid(passphrase, anagram=False):
+    known = set()
+
+    for word in passphrase.split():
+        if anagram:
+            word = ''.join(sorted(list(word)))
+        if word in known:
+            return False
+        known.add(word)
+    return True
+
+
+def part1(data):
+    return sum(is_valid(passphrase) for passphrase in data)
+
+
+def part2(data):
+    return sum(is_valid(passphrase, True) for passphrase in data)
+
+
+if __name__ == '__main__':
+    data = data_import('2017/data/day4')
+    print('Solution of 1 is', part1(data))
+    print('Solution of 2 is', part2(data))
