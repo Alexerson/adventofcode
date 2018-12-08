a8bd806dd5957154b3532390355403635f324070
Alexandre Spaeth <alexandre@e-180.com>
2018-12-08 15:01:33 +0100

    2017 day 6

---
+++ 2017/day6.py
@@ -0,0 +1,42 @@
+from utils import data_import
+
+
+def parts(data_):
+
+    total = sum(data_)
+
+    data = data_.copy()
+    known = {}
+
+    count = len(data)
+
+    cycle = 0
+
+    while tuple(data) not in known:
+        known[(tuple(data))] = cycle
+        cycle += 1
+        bank, content = max(enumerate(data), key=lambda a: (a[1], -a[0]))
+
+        data[bank] = 0
+        increment = content // count
+        rest = content % count
+
+        for index in range(count):
+            data[index] += increment
+
+        min_bonus = bank + 1
+        max_bonus = bank + rest
+
+        for index in range(min_bonus, max_bonus + 1):
+            data[index % count] += 1
+
+        assert sum(data) == total
+
+    return cycle, cycle - known[tuple(data)]
+
+
+if __name__ == '__main__':
+    data = data_import('2017/data/day6', int, True)[0]
+    part1, part2 = parts(data)
+    print('Solution of 1 is', part1)
+    print('Solution of 2 is', part2)
