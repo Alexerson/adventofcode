1d3849eb2455c44bce400dab9d84b80ce06eb6d5
Alexandre Spaeth <alexandre@e-180.com>
2018-12-13 14:30:34 +0100

    Add day 14 (seems my day10 solution is wrong)

---
+++ y2017/day10_alt.py
@@ -0,0 +1,39 @@
+from functools import reduce
+from itertools import accumulate
+from itertools import zip_longest as zipl
+from operator import mul, xor
+
+
+def reverse_sublist(l, a, b):
+    if a <= b:
+        l[a:b] = l[a:b][::-1]
+    else:
+        r = (l[a:] + l[:b])[::-1]
+        l[a:], l[:b] = r[: len(l) - a], r[-b or len(r) :]
+
+
+def hash_round(
+    lens, elems, pos=0, skip=0, accumulator=lambda x, y: (y[0], reduce(sum, x))
+):
+    for (skip, s), pos in accumulate(
+        zipl(enumerate(lens, skip), [pos]), accumulator
+    ):
+        reverse_sublist(elems, pos % len(elems), (pos + s) % len(elems))
+    return elems, skip + s + pos, skip + 1
+
+
+def solve1(input, n=256):
+    return mul(
+        *hash_round([int(l) for l in input.split(',')], list(range(n)))[0][:2]
+    )
+
+
+def solve2(
+    input, n=256, g=16, rounds=64, suffix=[17, 31, 73, 47, 23], pos=0, skip=0
+):
+    elems, lengths = [*range(n)], [ord(c) for c in input.strip()] + suffix
+    for _ in range(rounds):
+        elems, pos, skip = hash_round(lengths, elems, pos, skip)
+    return bytes(
+        reduce(xor, elems[g * k : g * (k + 1)]) for k in range(n // g)
+    ).hex()
---
+++ y2017/day14.py
@@ -0,0 +1,84 @@
+from utils import data_import
+from y2017.day10_alt import solve2 as knot_hash
+
+
+def part1(data):
+    output = []
+    for i in range(128):
+        input_ = '{}-{}'.format(data, i)
+
+        binary_hash = bin(int(knot_hash(input_), 16))[2:]
+
+        output.append(binary_hash.zfill(128))
+    return sum(d == '1' for line in output for d in line)
+
+
+def find_region(coord, output):
+    to_visit = set([coord])
+    visited = set()
+    match = set()
+
+    while len(to_visit):
+        cell = to_visit.pop()
+
+        visited.add(cell)
+
+        if output[cell[0]][cell[1]]:
+            match.add(cell)
+        else:
+            continue
+
+        for i, j in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            new_cell = (cell[0] + i, cell[1] + j)
+            if (
+                new_cell[0] < 0
+                or new_cell[1] < 0
+                or new_cell[0] > 127
+                or new_cell[1] > 127
+                or new_cell in visited
+            ):
+                continue
+            to_visit.add(new_cell)
+
+    return match
+
+
+def part2(data):
+    output = []
+    for i in range(128):
+        input_ = '{}-{}'.format(data, i)
+
+        binary_hash = bin(int(knot_hash(input_), 16))[2:].zfill(128)
+        output.append([d == '1' for d in binary_hash.zfill(128)])
+
+    regions = {}
+
+    region_count = 0
+
+    for line_i, line in enumerate(output):
+        for column_i, cell in enumerate(line):
+            if cell and (line_i, column_i) not in regions.keys():
+                region = find_region((line_i, column_i), output)
+                region_count += 1
+
+                for i, j in region:
+                    regions[(i, j)] = region_count
+
+    return region_count
+
+
+def show_regions(regions):
+    for i in range(8):
+        print(
+            ''.join(str(regions.get((i, j), '..')).zfill(2) for j in range(8))
+        )
+
+
+if __name__ == '__main__':
+    input_example = 'flqrgnkx'
+    print('Solution of 1 is', part1(input_example))
+    print('Solution of 2 is', part2(input_example))
+
+    data = 'hwlqcszp'
+    print('Solution of 1 is', part1(data))
+    print('Solution of 2 is', part2(data))
