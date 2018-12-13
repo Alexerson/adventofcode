e2b5ebfecd2e9c5ad8d5eff45a3557dc03629e87
Alexandre Spaeth <alexandre@e-180.com>
2018-12-13 12:14:21 +0100

    New utils changes

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
-def data_import(filename, cast=str, split_char=None):
+def data_import(filename, cast=str, split_char=None, rstrip=False):
     data = []
     with open(filename) as file:
         line = file.readline()
         while line:
-            if line.strip():
+            if rstrip and line.rstrip() or line.strip():
 
                 if split_char is not None:
                     line = line.split(split_char)
                     data.append([cast(item.strip()) for item in line])
                 else:
-                    data.append(cast(line.strip()))
+                    data.append(cast(rstrip and line.rstrip() or line.strip()))
 
             line = file.readline()
     return data
