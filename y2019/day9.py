8a346ab628bbafb61bdf6f0b6571816efe7eb101
Alexandre Spaeth <alexandre@e-180.com>
2019-12-08 21:40:29 -0800

    Day 9 2019

---
+++ y2019/day9.py
@@ -0,0 +1,16 @@
+from utils import data_import
+from intcode import Program
+
+def part1(data):
+    return list(Program(data).execute([1]))
+
+def part2(data):
+    return list(Program(data).execute([2]))
+
+
+if __name__ == '__main__':
+
+    data = data_import('y2019/data/day9', cast=int, split_char=',')[0]
+
+    print('Solution of 1 is', part1(data))
+    print('Solution of 2 is', part2(data))
--- y2019/intcode.py
+++ y2019/intcode.py
@@ -4,6 +4,8 @@ class Program(object):
 
     def __init__(self, memory):
         self.memory = list(memory)
+        for i in range(10000):
+            self.memory.append(0)
         self.pointer = 0
         self.finished = False
 
@@ -13,6 +15,8 @@ class Program(object):
         if inputs is None:
             inputs = []
 
+        self.relative_base = 0
+
         outputs = []
 
         while (opcode:=memory[self.pointer]) != 99:
@@ -20,31 +24,49 @@ class Program(object):
             instruction = opcode % 100
             parameter_modes = opcode // 100
 
-            if instruction in (1, 2, 7, 8):
-                params_count = 3
-            elif instruction in (3, 4):
+            if instruction in (3, 4, 9):
                 params_count = 1
             elif instruction in (5, 6):
                 params_count = 2
+            elif instruction in (1, 2, 7, 8):
+                params_count = 3
                 
             params = [
                 (
-                    memory[self.pointer + 1 + index], 
-                    (parameter_modes // (10**index)) % 10**(index+1) 
+                    memory[self.pointer + 1 + i], 
+                    (parameter_modes // (10**i)) % 10 
                 )
-                for index in range(params_count)
+                for i in range(params_count)
             ]
 
-            values = [param[0] if param[1] == 1 else memory[param[0]] for param in params]
+            values = []
+            for param in params:
+                if param[1] == 0:
+                    values.append(memory[param[0]])
+                elif param[1] == 1:
+                    values.append(param[0])
+                elif param[1] == 2:
+                    values.append(memory[param[0] + self.relative_base])
+                else:
+                    raise ValueError('Wrong mode')
 
             if instruction == 1:
-                memory[params[2][0]] = values[0] + values[1]
+                index_ = params[2][0]
+                if params[2][1] == 2:
+                    index_ += self.relative_base
+                memory[index_] = values[0] + values[1]
                 
             elif instruction == 2:
-                memory[params[2][0]] = values[0] * values[1]
+                index_ = params[2][0]
+                if params[2][1] == 2:
+                    index_ += self.relative_base
+                memory[index_] = values[0] * values[1]
 
             elif instruction == 3:
-                memory[params[0][0]] = inputs.pop(0)
+                index_ = params[0][0]
+                if params[0][1] == 2:
+                    index_ += self.relative_base
+                memory[index_] = inputs.pop(0)
 
             elif instruction == 4:
                 yield values[0]
@@ -58,10 +80,19 @@ class Program(object):
                     self.pointer = values[1] - 1 - params_count
 
             elif instruction == 7:
-                memory[params[2][0]] = int(values[0] < values[1])
+                index_ = params[2][0]
+                if params[2][1] == 2:
+                    index_ += self.relative_base
+                memory[index_] = int(values[0] < values[1])
 
             elif instruction == 8:
-                memory[params[2][0]] = int(values[0] == values[1])
+                index_ = params[2][0]
+                if params[2][1] == 2:
+                    index_ += self.relative_base
+                memory[index_] = int(values[0] == values[1])
+
+            elif instruction == 9:
+                self.relative_base += values[0]
 
             else:
                 raise ValueError("This should not happen")
