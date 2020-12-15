b274c4bc1c37b3df52058ef8991558be2bfb385e
Alexandre Spaeth <alexandre@e-180.com>
2020-12-15 14:10:27 -0800

    Day 15 2020

--- y2020/day15.py
+++ y2020/day15.py
@@ -3,28 +3,29 @@ def part1(data, limit) -> int:
     last_number = 0
 
     for turn, value in enumerate(data):
-        spoken[value] = [turn + 1]
+        spoken[value] = (None, turn + 1)
         last_number = value
 
-    for turn in range(len(data) + 1, limit + 1):
-        if last_number not in spoken:
-            last_number = 0
-        else:
-            if len(spoken[last_number]) == 1:
-                last_number = 0
-            else:
-                last_number = spoken[last_number][-1] - spoken[last_number][-2]
-        if last_number not in spoken:
-            spoken[last_number] = []
+    turn = len(data) + 1
 
-        spoken[last_number].append(turn)
+    while turn <= limit:
+        try:
+            last_number = spoken[last_number][1] - spoken[last_number][0]
+        except (TypeError, KeyError):
+            last_number = 0
+
+        if last_number in spoken:
+            spoken[last_number] = (spoken[last_number][1], turn)
+        else:
+            spoken[last_number] = (None, turn)
+        turn += 1
 
     return last_number
 
 
 if __name__ == '__main__':
-    mydata = [0, 3, 6]
-    # mydata = [1, 2, 16, 19, 18, 0]
+    # mydata = [0, 3, 6]
+    mydata = [1, 2, 16, 19, 18, 0]
 
     print('Data is: ', mydata)
 
