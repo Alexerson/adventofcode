4d91437ebfcc36c36854dc209dc150e67a6d8bda
Alexandre Spaeth <Alexerson@users.noreply.github.com>
2021-12-06 21:06:36 -0800

    2021 day 7

---
+++ y2021/day7.py
@@ -0,0 +1,33 @@
+from utils import data_import
+
+
+def part1(data: list[int]) -> int:
+
+    return min(
+        sum(abs(x - position) for x in data)
+        for position in range(min(data), max(data) + 1)
+    )
+
+
+def cost(x, y):
+    n = abs(x - y)
+    return n * (n + 1) // 2
+
+
+def part2(data: list[int]) -> int:
+    return min(
+        sum(cost(x, position) for x in data)
+        for position in range(min(data), max(data) + 1)
+    )
+
+
+if __name__ == '__main__':
+    mydata = data_import('y2021/data/day7_example', split_char=',', cast=int)[
+        0
+    ]
+    mydata = data_import('y2021/data/day7', split_char=',', cast=int)[0]
+
+    print('Input is', mydata)
+
+    print('Solution of 1 is', part1(mydata))
+    print('Solution of 2 is', part2(mydata))
