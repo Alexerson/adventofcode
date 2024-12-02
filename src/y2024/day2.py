53fe7d14d2e5668566748550adc5243402371c3d
Alexandre Spaeth <Alexerson@users.noreply.github.com>
2024-12-02 13:33:37 -0800

    Slighly improve day2

--- src/y2024/day2.py
+++ src/y2024/day2.py
@@ -2,16 +2,17 @@ from utils import data_import
 
 
 def is_safe(levels: list[int]) -> bool:
-    # All increasing or decreasing:
-    if levels != sorted(levels) and levels != sorted(levels, reverse=True):
-        return False
+    increasing = None
 
     current = levels[0]
     for next in levels[1:]:
-        if next == current:
+        if increasing is None:
+            increasing = next > current
+
+        if increasing != (next > current):
             return False
 
-        if abs(next - current) > 3:
+        if not (1 <= abs(next - current) <= 3):
             return False
 
         current = next
@@ -19,30 +20,26 @@ def is_safe(levels: list[int]) -> bool:
     return True
 
 
+def is_safe_tolerant(levels: list[int]) -> bool:
+    if is_safe(levels):
+        return True
+
+    for i in range(len(levels)):
+        if is_safe(levels[:i] + levels[i + 1 :]):
+            return True
+
+    return False
+
+
 def part1(data: list[list[int]]) -> int:
     return sum(is_safe(d) for d in data)
 
 
 def part2(data: list[list[int]]) -> int:
-    total_safe = 0
-
-    for levels in data:
-        if is_safe(levels):
-            total_safe += 1
-            print(levels, 'safe')
-            continue
-
-        for i in range(len(levels)):
-            if is_safe(levels[:i] + levels[i + 1 :]):
-                print(levels, 'safe if removing', levels[i])
-                total_safe += 1
-                break
-
-    return total_safe
+    return sum(is_safe_tolerant(d) for d in data)
 
 
 if __name__ == '__main__':
     mydata = data_import('data/y2024/day2', int, split_char=' ')
-    # print('Input is', mydata)
     print('Solution of 1 is', part1(mydata))
     print('Solution of 2 is', part2(mydata))
